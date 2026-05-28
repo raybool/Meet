aeb5872a168a27a9ed27aa70d327391ba6350549
Raybool <vavilovs02@rambler.ru>
2026-05-28 12:37:14 +0500

    feat: add Vercel Analytics and Speed Insights for performance tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
+      <SpeedInsights />
+      
       <body>{children}</body>
     </html>
   );
